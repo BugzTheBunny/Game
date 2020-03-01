3a8501132a79e0ae0ab8c236188d5eebac099e1b
Slava <urigami2010@gmail.com>
2020-03-01 16:19:45 +0200

    Ground + ground collision

--- Player.py
+++ Player.py
@@ -1,20 +1,21 @@
 from kivy.uix.widget import Widget
 from kivy.graphics import Rectangle
-from kivy.graphics import RoundedRectangle
+from kivy.graphics import Triangle
 from kivy.core.window import Window
 from kivy.clock import Clock
 import random
 
-def collide(player,enemy):
+
+def collide(player, object):
     p_x = player[0][0]
     p_y = player[0][1]
-    e_x = enemy[0][0]
-    e_y = enemy[0][1]
+    e_x = object[0][0]
+    e_y = object[0][1]
 
     p_w = player[1][0]
     p_h = player[1][1]
-    e_w = enemy[1][0]
-    e_h = enemy[1][1]
+    e_w = object[1][0]
+    e_h = object[1][1]
 
     if (p_x < e_x + e_w and
             p_x + p_w > e_x and
@@ -23,6 +24,8 @@ def collide(player,enemy):
         return True
     else:
         return False
+
+
 class GameWidget(Widget):
     """
     This will be the game widget.
@@ -33,11 +36,12 @@ class GameWidget(Widget):
         self._keyboard = Window.request_keyboard(self._on_keyboard_close, self)
         self._keyboard.bind(on_key_down=self._on_key_down)
         self._keyboard.bind(on_key_up=self._on_key_up)
+
         with self.canvas:
-            self.player = Rectangle(pos=(200, 200), size=(20, 30))
-            self.enemy = Rectangle(pos=(500,500), size = (50, 50))
-            # If image:
-            # Rectangle(source="player.png", size=(100, 50))
+            self.ground = Rectangle(pos=(0, 0), size=(800, 40))
+            self.player = Rectangle(source="player.png", pos=(0, self.ground.size[1]), size=(48, 40))
+            self.trap = Triangle(pos=(5, 5))
+            self.enemy = Rectangle(pos=(100, 100), size=(50, 50))
 
         self.keysPressed = set()
         gravity = 0
@@ -66,11 +70,11 @@ class GameWidget(Widget):
         currentx = self.player.pos[0]
         currenty = self.player.pos[1]
 
-        step_size = 200 * dt
-        # Movement.
+        step_size = 100 * dt
+
         if "w" in self.keysPressed:
-            gravity = 0
-            currenty += step_size * 2.5
+            self.player.source = 'player_jump.png'
+            currenty += step_size * 10
         if "s" in self.keysPressed:
             currenty -= step_size
         if "a" in self.keysPressed:
@@ -79,12 +83,14 @@ class GameWidget(Widget):
             currentx += step_size
         self.player.pos = (currentx, currenty)
 
-        # if self.player.pos == self.enemy.pos:
-        #     print('Collide')
         # Gravity (If you wont press W)
         if 'w' not in self.keysPressed:
-            currenty -= random.randint(3,7)
+            currenty -= random.randint(3, 7)
             self.player.pos = (currentx, currenty)
+            self.player.source = 'player.png'
 
-        if collide((self.player.pos, self.player.size),(self.enemy.pos,self.enemy.size)):
-            self.enemy.pos = (random.random(100,800),random.random(100,800))
+        if collide((self.player.pos, self.player.size), (self.enemy.pos, self.enemy.size)):
+            self.enemy.pos = (random.randint(0, 500), random.randint(0, 500))
+
+        if collide((self.player.pos, self.player.size), (self.ground.pos, self.ground.size)):
+            self.player.pos = (currentx, self.ground.size[1])
